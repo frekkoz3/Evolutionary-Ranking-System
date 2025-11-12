fc6bc1d32ebded4e2bf5a81c9707ff5efd97cb9a
frekkoz3 <francescobredariol03@gmail.com>
2025-11-12 14:32:32 +0100

    Modified a little the comments in individual.py

--- source/individual.py
+++ source/individual.py
@@ -61,6 +61,11 @@ class RandomIndividual(Individual):
         return random.choice(game.valid_moves())
     
 class GeneticPolicyIndividual(Individual):
+    """
+        This class should be the implementation of the individual we actually want to implement for the Evolutionary Ranking System project.
+        It should presents all what it needs to learn, mutate etc.
+        It should use the utilities from the evo_utils.py and it should use the policy as an attribute.
+    """
 
     def __init__(self, initial_policy, init_elo = 100):
         super().__init__(init_elo)
